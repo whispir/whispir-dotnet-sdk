a9a4d2c74d1164e1df77b021d8d200426f3ee54f
Sanjeev <smandalapu@whispir.com>
2018-12-03 14:32:39 +1100

    Updates to the Program.cs to show usage examples.

--- Whispir.Messaging.ConsoleSample/Program.cs
+++ Whispir.Messaging.ConsoleSample/Program.cs
@@ -15,7 +15,7 @@ namespace Whispir.Messaging.ConsoleSample
             IMessageService service = new MessageService(new WhispirSettings()
             {
                 ApiAuthorization = AppConfig.WhispirAuthorization,
-                APIEndpoint = SDK.Enums.APIEndpoints.AU,
+                APIEndpoint = SDK.Enums.APIEndpoints.IT,
                 ApiKey = AppConfig.WhispirApiKey,
                 LoggingFolder = @"E:\Logs",
                 DataBaseFolder = @"E:\Logs",
@@ -24,14 +24,14 @@ namespace Whispir.Messaging.ConsoleSample
 
             // Samples 
             // Simple SMS 
-            IMessage sms = new SMS() { To = "+61407211770", Subject = "Simple Test", Content = "Simple Test",CallBackID= "callbackName" };
-            var result = await service.SendMessageAsync(sms);
+            // IMessage sms = new SMS() { To = "+61Phone", Subject = "Simple Test", Content = "Simple Test", CallBackID = "callback_name" };
+            //var result = await service.SendMessageAsync(sms);
 
             //sms = new SMS() { To = "+61phone", Subject = "Simple Test-2", Content = "Simple Test-2" };
             //result = await service.SendMessageAsync(sms);
 
             //// Simple SMS To wrong Number
-            //sms = new SMS() { To = "+009889897654321", Subject = "Simple Test", Content = "Simple Test-Wrong Number" };
+            //sms = new SMS() { To = "+61phone", Subject = "Simple Test", Content = "Simple Test-Wrong Number" };
             //result = await service.SendMessageAsync(sms);
 
             // get the Messages Sent
@@ -41,14 +41,17 @@ namespace Whispir.Messaging.ConsoleSample
             //{
             //    var msgresponse = (MessageSentResponse)await service.GetMessages(msg.ID);
             //    // print value of each attribute
-            //    foreach(var attr in msgresponse.messageattributes.attribute)
+            //    if (msgresponse != null && msgresponse.messageattributes != null)
             //    {
-            //        Console.WriteLine("Attribute:{0}={1}", attr.name, attr.value);
+            //        foreach (var attr in msgresponse.messageattributes.attribute)
+            //        {
+            //            Console.WriteLine("Attribute:{0}={1}", attr.name, attr.value);
+            //        }
+            //        MessagesSent.Add(msgresponse);
             //    }
-            //    MessagesSent.Add(msgresponse);
             //}
 
-            //// Now get the status of the messages sent in the last 24 hours
+            // Now get the status of the messages sent in the last 24 hours
             //List<MessageStatusResponse> messageStatus = new List<MessageStatusResponse>();
             //var Messages = await service.GetMessagesAsync();
             //foreach (var msg in Messages)
@@ -56,23 +59,24 @@ namespace Whispir.Messaging.ConsoleSample
             //    var msgresponse = await service.GetMessageStatus(msg.ID);
             //    // write to Log
             //    Console.WriteLine(msgresponse);
-            //    Console.ReadLine();
+            //    //Console.ReadLine();
+            //   // await service.SetMessageAsProcessed(msg.ID);
             //}
 
-            //// Get Responses from the messages if any
+            // Get Responses from the messages if any
             //List<MessageResponseResponse> responses = new List<MessageResponseResponse>();
             //var MessageResponses = await service.GetMessagesAsync();
             //foreach (var msg in MessageResponses)
             //{
             //    var msgresponse = await service.GetMessageResponse(msg.ID);
-            //    Console.WriteLine(msgresponse);
-            //    Console.ReadLine();
+            //    Console.WriteLine($"Reply : {msgresponse}");
+                
             //    //await service.SetMessageAsprocessed(msg.ID));
             //}
 
             // Get Templates
             //List<TemplatesResponse> TemplateResponse = new List<TemplatesResponse>();
-            //var Templates = (TemplatesResponse) await service.GetTemplates();
+            //var Templates = (TemplatesResponse)await service.GetTemplates();
             //foreach (var template in Templates.messagetemplates)
             //{
             //    Console.Write(template.id);
@@ -85,20 +89,11 @@ namespace Whispir.Messaging.ConsoleSample
             //    Mytemplate.Attributes = new List<MessageAttribute>();
             //    Mytemplate.Attributes.Add(new MessageAttribute() { Name = "attribute1", Value = DateTime.Now.ToString("hh:mm tt") });
             //    Mytemplate.Attributes.Add(new MessageAttribute() { Name = "attribute2", Value = DateTime.Now.ToString("dd/MM/yyyy") });
-            //    IMessage sms = new SMS() { To = "+61407211770", Subject = "Test Template Message", MessageTemplate = Mytemplate };
+            //    IMessage sms = new SMS() { To = "+61phone", Subject = "Test Template Message", MessageTemplate = Mytemplate };
             //    var Templateresult = await service.SendMessageAsync(sms);
             //}
 
-            // get CallBacks
-            var CallBacks = await service.GetCallBacks();
-           // You can Get the call information by iterating the calls
-            foreach (var call in CallBacks)
-            {
-                var responseMessage = call.responseMessage.content;
-            }
-            // To Update calls with the status to Success, send a List<Calls> to the updateCallStatus
-            // will return OK if the update was successfull or null if it failed.
-              var status = await service.updateCallStatus(CallBacks);
+            //Console.ReadLine();
         }
 
         static void Main(string[] args)
@@ -116,7 +111,6 @@ namespace Whispir.Messaging.ConsoleSample
     public static class AppConfig
     {
         public static string WhispirApiKey = ConfigurationManager.AppSettings["WhispirApiKey"];
-        public static string WhispirApiUrl = ConfigurationManager.AppSettings["WhispirApiUrl"];
         public static string WhispirAuthorization = ConfigurationManager.AppSettings["WhispirApiAuthorization"];
         public static int LoggingHours = Convert.ToInt32(ConfigurationManager.AppSettings["LoggingHours"]);
     }
